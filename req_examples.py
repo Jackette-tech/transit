af86397638b872dcb459537e9c395edaf6c1d938
scordd <savarozsnyai@gmail.com>
2022-10-09 19:53:46 -0700

    whoops

--- req_examples.py
+++ req_examples.py
@@ -11,8 +11,6 @@ params = {'apikey': key}
 headers = {'application': 'json'}
 
 response = requests.get(base + stops + stop, params=params)
-# response = requests.get(base + 'stops/' + '51658' + '?apikey=9maK7GrmRhkrTvusGQfF' + '\n')
-# response = requests.get('https://api.translink.ca/rttiapi/v1/stops/51658?apikey=9maK7GrmRhkrTvusGQfF')
 
 with open('output.txt', 'a') as output:
     output.write(response.text + '\n')
